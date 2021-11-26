19623ea7569223d5349972960c6fc3d49108bad8
Ajay U <ajayudayasanu95@gmail.com>
2021-11-26 23:50:45 +0530

    netfly form submission change 4

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+const encode = (data) => {
+   return Object.keys(data)
+       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+       .join("&");
+ }
 class Contact extends Component {
    constructor(props) {
       super(props);
